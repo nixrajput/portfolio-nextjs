66b50db282518cd1c241869bfd684d1df7d68cd9
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2022-02-24 03:44:50 +0530

    animation changes and layout fixes

--- components/SocialMedia/SocialMedia.js
+++ components/SocialMedia/SocialMedia.js
@@ -7,24 +7,28 @@ const SocialMedia = () => {
 
             <a href='https://linkedin.com/in/nixrajput'
                 target='_blank'
+                rel="noreferrer"
             >
                 <BsLinkedin />
             </a>
 
             <a href='https://instagram.com/nixrajput'
                 target='_blank'
+                rel="noreferrer"
             >
                 <BsInstagram />
             </a>
 
             <a href='https://twitter.com/nixrajput07'
                 target='_blank'
+                rel="noreferrer"
             >
                 <BsTwitter />
             </a>
 
             <a href='https://facebook.com/nixrajput07'
                 target='_blank'
+                rel="noreferrer"
             >
                 <FaFacebookF />
             </a>
