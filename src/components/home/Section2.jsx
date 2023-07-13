aac0e101a23f34289b61b54d6c59bd50e0652de0
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2023-07-13 14:57:24 +0530

    update changes and bug fixes

--- src/components/home/Section2.jsx
+++ src/components/home/Section2.jsx
@@ -7,19 +7,19 @@ import Column from "@/components/common/Column";
 const services = [
   {
     title: "Mobile App Development",
-    image: "/icons/mobile-dev.png",
+    image: "/icons/mobile-dev.webp",
     description:
       "I specialize in creating captivating mobile applications that engage your audience. From concept to deployment, I develop native and cross-platform apps for iOS and Android. With cutting-edge technologies, I ensure seamless performance, intuitive interfaces, and robust functionality that aligns perfectly with your business goals.",
   },
   {
     title: "Web Development",
-    image: "/icons/web-dev.png",
+    image: "/icons/web-dev.webp",
     description:
       "I deliver visually stunning and user-friendly websites to help you establish a strong online presence. Whether it's a simple informational site or a complex e-commerce platform, I provide tailored web development solutions. Using the latest frameworks and technologies, I create responsive, SEO-friendly websites that offer a seamless browsing experience on all devices.",
   },
   {
     title: "Backend Development",
-    image: "/icons/backend-dev.png",
+    image: "/icons/backend-dev.webp",
     description:
       "I enhance your digital applications with a robust and scalable backend infrastructure. With expertise in backend development, I create efficient database structures, develop APIs, and configure servers to ensure optimal performance, security, and scalability. My solutions empower your applications to seamlessly handle high traffic and complex data management.",
   },
@@ -42,7 +42,7 @@ const HomeSection2 = () => {
               >
                 <Image
                   src={service.image}
-                  alt="profile"
+                  alt={`service-${index}`}
                   width={100}
                   height={100}
                   sizes="100%"
