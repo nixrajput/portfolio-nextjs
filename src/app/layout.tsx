178af148fcca561ea00ad429ac1f6975e9711826
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2024-05-10 12:59:40 +0530

    update changes and bug fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import LocalConfig from "@/constants/config";
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   style: ["normal", "italic"],
-  subsets: ["devanagari", "latin"],
+  subsets: ["latin", "latin-ext"],
   display: "swap",
   preload: true,
   fallback: [
