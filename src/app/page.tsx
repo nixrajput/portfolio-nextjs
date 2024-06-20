9c7d939265cc834a383b6e23084e9e2984b808c7
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2024-06-20 22:35:39 +0530

    Update Changes & Bug Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,46 +3,28 @@ import dynamic from "next/dynamic";
 import PageBox from "@/components/core/PageBox";
 import { menuItems } from "@/data/navMenus";
 
-const FloatingNav = dynamic(() => import("@/components/navbar/NavbarNew"), {
-  ssr: false,
-});
+const FloatingNav = dynamic(() => import("@/components/navbar/NavbarNew"));
 
-const HomeSection1 = dynamic(() => import("@/components/home/Section1"), {
-  ssr: false,
-});
-const HomeSection2 = dynamic(() => import("@/components/home/Section2"), {
-  ssr: false,
-});
-const HomeSection3 = dynamic(() => import("@/components/home/Section3"), {
-  ssr: false,
-});
-const HomeSection4 = dynamic(() => import("@/components/home/Section4"), {
-  ssr: false,
-});
-const HomeSection5 = dynamic(() => import("@/components/home/Section5"), {
-  ssr: false,
-});
-const HomeSection6 = dynamic(() => import("@/components/home/Section6"), {
-  ssr: false,
-});
+const HomeSection1 = dynamic(() => import("@/components/home/Section1"));
+const HomeSection2 = dynamic(() => import("@/components/home/Section2"));
+const HomeSection3 = dynamic(() => import("@/components/home/Section3"));
+const HomeSection4 = dynamic(() => import("@/components/home/Section4"));
+const HomeSection5 = dynamic(() => import("@/components/home/Section5"));
+const HomeSection6 = dynamic(() => import("@/components/home/Section6"));
 
-const ScrollToTop = dynamic(() => import("@/components/common/ScrollToTop"), {
-  ssr: false,
-});
+const ScrollToTop = dynamic(() => import("@/components/common/ScrollToTop"));
 
 const Home = () => {
   return (
     <PageBox>
-      <Suspense>
-        <FloatingNav className="app_nav" navItems={menuItems} />
-        <HomeSection1 id="hero" />
-        <HomeSection2 id="services" />
-        <HomeSection3 id="experiences" />
-        <HomeSection4 id="skills" />
-        <HomeSection5 id="projects" />
-        <HomeSection6 id="contact" />
-        <ScrollToTop />
-      </Suspense>
+      <FloatingNav className="app_nav" navItems={menuItems} />
+      <HomeSection1 id="hero" />
+      <HomeSection2 id="services" />
+      <HomeSection3 id="experiences" />
+      <HomeSection4 id="skills" />
+      <HomeSection5 id="projects" />
+      <HomeSection6 id="contact" />
+      <ScrollToTop />
     </PageBox>
   );
 };
