6b30db72aab2963b4baf0c5abd2df397acfc3f33
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2024-06-20 23:02:14 +0530

    Update Changes & Bug Fixes

--- src/components/home/Section1.tsx
+++ src/components/home/Section1.tsx
@@ -74,12 +74,12 @@ const HomeSection1 = ({ id }: Readonly<{ id: string }>) => {
 
   return (
     <ResponsiveBox
-      classNames="dark:bg-[var(--dialogColor)] bg-[var(--dialogColor)] dark:bg-grid-small-white/[0.2] bg-grid-small-white/[0.2] min-h-screen items-center justify-center relative overflow-hidden rounded-md"
+      classNames="dark:bg-[var(--bgColor)] bg-[var(--bgColor)] dark:bg-grid-small-white/[0.2] bg-grid-small-white/[0.2] min-h-screen items-center justify-center relative overflow-hidden rounded-md"
       id={id}
     >
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
-      <ConstraintedBox classNames="px-4 py-8 z-10 w-full items-center justify-center">
+      <ConstraintedBox classNames="px-4 py-8 z-20 items-center justify-center">
         <Column classNames="w-full items-center justify-center">
           <TypewriterEffectSmooth words={text_1} delay={0} />
           <TypewriterEffectSmooth
--- src/components/home/Section2.tsx
+++ src/components/home/Section2.tsx
@@ -8,10 +8,12 @@ import services from "@/data/services";
 const HomeSection2 = ({ id }: { id: string }) => {
   return (
     <ResponsiveBox
-      classNames="min-h-[calc(100vh-5rem)] items-center justify-center"
+      classNames="dark:bg-[var(--dialogColor)] bg-[var(--dialogColor)] min-h-screen items-center justify-center dark:bg-dot-white/[0.2] bg-dot-white/[0.2] overflow-hidden rounded-md"
       id={id}
     >
-      <ConstraintedBox classNames="p-4 py-16">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+
+      <ConstraintedBox classNames="p-4 py-16 z-10">
         <SectionTitle>My Services</SectionTitle>
 
         <GridBox classNames="justify-items-center sm:grid-cols-2 lg:grid-cols-3 mt-16">
