4a4ec8a95068cd416de9d4af30224d5febe29cc6
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2024-06-27 14:24:40 +0530

    Update Changes & Bug Fixes

--- src/components/navbar/NavbarNew.tsx
+++ src/components/navbar/NavbarNew.tsx
@@ -29,7 +29,7 @@ const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit fixed top-4 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full bg-[var(--dialogColor)] shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2 items-center justify-center space-x-4",
+          "flex max-w-fit fixed top-4 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full bg-[var(--dialogColor50)] backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2 items-center justify-center space-x-4",
           className
         )}
       >
@@ -38,7 +38,7 @@ const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+              "relative items-center flex space-x-1 text-neutral-50 hover:text-neutral-300"
             )}
           >
             <span className="block sm:hidden">
@@ -56,7 +56,7 @@ const FloatingNav = ({
         <Link
           href="https://github.com/sponsors/nixrajput"
           target="_blank"
-          className="border text-xs/none sm:text-sm/none font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white hover:text-[var(--primaryColor)] px-4 py-2 rounded-full"
+          className="border text-xs/none sm:text-sm/none font-medium relative border-white/[0.2] text-white hover:text-[var(--primaryColor)] px-4 py-2 rounded-full"
         >
           <span className="block sm:hidden">
             <FontAwesomeIcon icon={faHandHoldingDollar} title="Support Me" />
