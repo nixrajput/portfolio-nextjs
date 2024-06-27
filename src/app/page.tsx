4b795150e624445ce7b1d226ae8cc6860e2e7303
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2024-06-27 17:13:45 +0530

    Update Changes & Bug Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import dynamic from "next/dynamic";
 import PageBox from "@/components/core/PageBox";
 import { menuItems } from "@/data/navMenus";
-import FloatingNavbar from "@/components/navbar/FloatingNavbar";
 import HomeSection1 from "@/components/home/Section1";
 
 const HomeSection2 = dynamic(() => import("@/components/home/Section2"));
@@ -10,6 +9,9 @@ const HomeSection4 = dynamic(() => import("@/components/home/Section4"));
 const HomeSection5 = dynamic(() => import("@/components/home/Section5"));
 const HomeSection6 = dynamic(() => import("@/components/home/Section6"));
 
+const FloatingNavbar = dynamic(() =>
+  import("@/components/navbar/FloatingNavbar")
+);
 const ScrollToTop = dynamic(() => import("@/components/common/ScrollToTop"));
 
 const Home = () => {
